198e8bba9553562612477ec157a4846dff5befd9
grupunzel <118299224+grupunzel@users.noreply.github.com>
2024-06-07 13:36:35 +0300

    Add files via upload

---
+++ DZ/js/script.js
@@ -0,0 +1,9 @@
+function change_to_light() {
+    body.style.backgroundColor = "white";
+    texth2.style.color = "black";
+}
+
+function change_to_dark() {
+    body.style.backgroundColor = "black";
+    texth2.style.color = "white";
+}
